1a3c800bb8c255006e979860c674639afcfad5ed
Josecp03 <josecorrochanop2003@gmail.com>
2024-06-23 22:02:02 +0200

    Sentencia When

--- AndroidStudio/SentenciaWhen.kt
+++ AndroidStudio/SentenciaWhen.kt
@@ -6,6 +6,7 @@ fun main () {
 
 private fun sentenciaWhen() {
     val pais = "España"
+    val edad = 100
 
     when(pais) {
         "España" -> {
@@ -18,4 +19,21 @@ private fun sentenciaWhen() {
             println("No se conoce el idioma")
         }
     }
+
+    when(edad) {
+        0, 1, 2 -> {
+            println("Eres un bebé")
+        } in 3..10 -> {
+            println("Eres un niño")
+        } in 11..17 -> {
+            println("Eres un adolescente")
+        } in 18..69  -> {
+            println("Eres un adulto")
+        } in 70..99 -> {
+            println("Eres un anciano")
+        } else -> {
+            println("\uD83D\uDE00")
+        }
+    }
+
 }
